7f52f049f995b5920a63d8d03b0843072bd261f7
Oleksii <oleksii.dashchuk@lnu.edu.ua>
2025-03-05 21:39:14 +0200

    Box-shadow

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,41 +1,14 @@
-// Обираємо всі елементи для анімації
-const elements = document.querySelectorAll('.aboutus');
-
-// Налаштування Observer
-const options = {
-  threshold: 0.5
-};
-
-// Створюємо Observer
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, options);
-
-// Спостерігаємо за елементами
-elements.forEach(element => {
-  observer.observe(element);
-});
-window.addEventListener("scroll", function () {
-  var boxes = document.querySelectorAll('.big-box, .box-1, .box-2, .box-3, .box-4');
-  
-  boxes.forEach(function(box) {
-    var rect = box.getBoundingClientRect();
-    
-    
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-      box.classList.add("highlight");
-    } else {
-      box.classList.remove("highlight");
-    }
-  });
+// Default header starts as transparent
+window.addEventListener("DOMContentLoaded", function() {
+  var header = document.querySelector('#header-container');
+  header.classList.add("transparent");
+  document.getElementById("logoforchange").src = "/assets/images/common/white-logo.svg";
 });
 
+// Scroll event listener for header style changes
 window.addEventListener("scroll", function () {
   var header = document.querySelector('#header-container');
+  
   if (window.scrollY > 150) {
     header.classList.add("scrolled");
     header.classList.remove("transparent");
@@ -48,3 +21,36 @@ window.addEventListener("scroll", function () {
   }
 });
 
+// Visibility Observer for .aboutus elements
+const elements = document.querySelectorAll('.aboutus');
+const options = {
+  threshold: 0.5
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, options);
+
+// Observe all .aboutus elements
+elements.forEach(element => {
+  observer.observe(element);
+});
+
+// Highlight boxes on scroll
+window.addEventListener("scroll", function () {
+  var boxes = document.querySelectorAll('.big-box, .box-1, .box-2, .box-3, .box-4');
+  
+  boxes.forEach(function(box) {
+    var rect = box.getBoundingClientRect();
+    
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      box.classList.add("highlight");
+    } else {
+      box.classList.remove("highlight");
+    }
+  });
+});
